788b7f62f8e666f84de6ac00a1ecbf399ee845ae
OlaoyeVic <you@example.com>
2022-08-22 16:56:41 +0100

    final update

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,9 +1,14 @@
 import React from "react";
 import './styles/progress.scss'
 import { NavLink } from "react-router-dom";
+import { useLocation } from "react-router-dom"
 import PersonalInfo from "../pages/PersonalInfo";
 
 const Progress = () => {
+    const [isActive, setIsActive] = React.useState(false)
+    const location = useLocation()
+    const { pathname } = location
+    const splitLocation = pathname.split("/")
     const steps = [
         {
             page: "Personal Info",
@@ -19,15 +24,23 @@ const Progress = () => {
         }
     ]
 
-    const handleStop = (event) => {
+    const handleClick = (event) => {
         event.preventDefault()
+        setIsActive(current => !current)
     }
     return (
         <div className="progress">
             <div className="pages">
                 {steps.map((step, index) => (
                     <>
-                        <NavLink activeClassName="active" key={index} to={step.path}>
+                        <NavLink
+                        // className={isActive ? '.active::after': ''}
+                        // activeClassName="active"
+                        className={{"active" : "" === splitLocation[1] ? "active" : "billing-info" === splitLocation[1] ? "active" : "confirm-payment" === splitLocation[1]}} 
+                        key={index} 
+                        to={step.path}
+                        onClick={handleClick}
+                        >
                             <div>{step.page}</div>
                         </NavLink>
                     </>
